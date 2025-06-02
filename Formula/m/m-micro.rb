ac61f5e9bc26077d66e036a3328c01fa519f0056
commitmaniac <201806046+commitmaniac@users.noreply.github.com>
2025-06-02 06:12:36 +0000

    m-micro: add 2.0.14 bottle.

--- Formula/m/m-micro.rb
+++ Formula/m/m-micro.rb
@@ -7,6 +7,14 @@ class MMicro < Formula
   license "MIT"
   head "https://github.com/zyedidia/micro.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/commitmaniac/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1b24ae7e8e058513d248dff33d92205611c6de9b8c01358f5918644cb1c3d175"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8e9ce388d95e86b2b73588029fe9a9701ebe1e5125253ebc9a4c2ad745a72e14"
+    sha256 cellar: :any_skip_relocation, ventura:       "3daefab46d595781e6fcda5797916f0e6070dea8b3773a89b52ee360395e9902"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b63d8eeba1ee0d69eccb45b5e651324185f08b4dc80e4edf50c5b530e38902b1"
+  end
+
   depends_on "go" => :build
 
   conflicts_with "micro", because: "micro install a conflicting micro binary"
