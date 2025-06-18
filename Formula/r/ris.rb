b81ffa1227e97a885d05c9ead39d47fe83e98888
commitmaniac <201806046+commitmaniac@users.noreply.github.com>
2025-06-18 04:12:03 +0000

    ris: add 1.1.0 bottle.

--- Formula/r/ris.rb
+++ Formula/r/ris.rb
@@ -6,6 +6,14 @@ class Ris < Formula
   license "BSD-3-Clause"
   head "https://github.com/commitmaniac/ris.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/commitmaniac/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "57927bffb64e08b5ddfcc82be85f443083936b7e5b62e82f6b2b9034a4143213"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "57927bffb64e08b5ddfcc82be85f443083936b7e5b62e82f6b2b9034a4143213"
+    sha256 cellar: :any_skip_relocation, ventura:       "c0f9346305de8c3faf733716f36e6749adcc8ef4370ae041f7c5a5ba0323e256"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0a9d99d925ac7b158869e447fb6c1e5892cb17f4a6ff4cfd76d62ff4fa27aac3"
+  end
+
   depends_on "go" => :build
 
   def install
