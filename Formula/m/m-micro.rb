b9b4662fb016fb7b5571cde990b0aa33ca7e2ec3
commitmaniac <201806046+commitmaniac@users.noreply.github.com>
2025-06-23 01:42:46 -0400

    micro-syntax 1.1.0

--- Formula/m/m-micro.rb
+++ Formula/m/m-micro.rb
@@ -5,14 +5,15 @@ class MMicro < Formula
       tag:      "v2.0.14",
       revision: "04c577049ca898f097cd6a2dae69af0b4d4493e1"
   license "MIT"
+  revision 1
   head "https://github.com/zyedidia/micro.git", branch: "master"
 
   bottle do
     root_url "https://ghcr.io/v2/commitmaniac/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1b24ae7e8e058513d248dff33d92205611c6de9b8c01358f5918644cb1c3d175"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8e9ce388d95e86b2b73588029fe9a9701ebe1e5125253ebc9a4c2ad745a72e14"
-    sha256 cellar: :any_skip_relocation, ventura:       "3daefab46d595781e6fcda5797916f0e6070dea8b3773a89b52ee360395e9902"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b63d8eeba1ee0d69eccb45b5e651324185f08b4dc80e4edf50c5b530e38902b1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "49ca9a34b7d5c6029ecc7f3e37bdbbab278df209809509fba793fa425ee78fcf"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a0948107c6d679f3d44c9ff90b066cfce8fa0d412f4dfdc36974cb156d76d488"
+    sha256 cellar: :any_skip_relocation, ventura:       "0a7a57a8c9d04aa1b39e803d27cc898acb31cbe0096aac8f751fe24d8bf7a8ee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f62b7bc092195fdcd093d195885c633cfb3ba40215940555926700dc5879c6c8"
   end
 
   depends_on "go" => :build
@@ -20,13 +21,13 @@ class MMicro < Formula
   conflicts_with "micro", because: "micro install a conflicting micro binary"
 
   resource "micro-syntax" do
-    url "https://github.com/commitmaniac/micro-syntax/archive/refs/tags/1.0.0.tar.gz"
-    sha256 "d5a896005bbda14fbd10f1815cebdbad68d0041e1e02f703fa4e4823078751f2"
+    url "https://github.com/commitmaniac/micro-syntax/archive/refs/tags/1.1.0.tar.gz"
+    sha256 "d5ea23be8cb022f3776f1d21c3838a1f450421a8b55bac1de8b397bbf81db40a"
   end
 
   patch do
-    url "https://gist.github.com/commitmaniac/ce1bfd39705ed265181db82b8bdf76ec/raw/support-ublock-in-comment-plugin.patch"
-    sha256 "38c46923288d1edf5f893429c0841e3e71e009cfda79e0cf80a7ab3b6dd9f3d5"
+    url "https://gist.github.com/commitmaniac/ce1bfd39705ed265181db82b8bdf76ec/raw/adblock-list-support-comments.patch"
+    sha256 "975327728c7bd8f9cfcb37fbdb085113dc845c4f354a8ab3642c7280b4320626"
   end
 
   def install
